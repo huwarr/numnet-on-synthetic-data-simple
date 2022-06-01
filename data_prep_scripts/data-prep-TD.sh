9f8e3058de539a40f60d4ea75a5826b682222466
huwarr <mikhalmatova@gmail.com>
2022-06-01 21:27:11 +0300

    data preparation to seperate scripts

---
+++ data_prep_scripts/data-prep-TD.sh
@@ -0,0 +1,32 @@
+#!/bin/env bash
+#SBATCH --job-name=roberta_numnet_plus
+source activate numnet_venv_1_0
+
+set -xe
+
+TMSPAN=$1
+DATA_DIR=$2
+
+BASE_DIR=.
+
+CODE_DIR=${BASE_DIR}
+
+if [ ${TMSPAN} = tag_mspan ]; then
+  echo "Use tag_mspan model..."
+  CACHED_TRAIN=${DATA_DIR}/tmspan_cached_roberta_textual_train.pkl
+  CACHED_DEV=${DATA_DIR}/tmspan_cached_roberta_textual_dev.pkl
+  MODEL_CONFIG="--tag_mspan"
+  if [ \( ! -e "${CACHED_TRAIN}" \)  -o \( ! -e "${CACHED_DEV}" \) ]; then
+  echo "Preparing cached data."
+  python prepare_roberta_data.py --data_type textual --roberta_path ${DATA_DIR}/roberta.large --input_path ${DATA_DIR} --output_dir ${DATA_DIR} --tag_mspan
+  fi
+else
+  echo "Use mspan model..."
+  CACHED_TRAIN=${DATA_DIR}/cached_roberta_textual_train.pkl
+  CACHED_DEV=${DATA_DIR}/cached_roberta_textual_dev.pkl
+  MODEL_CONFIG=""
+  if [ \( ! -e "${CACHED_TRAIN}" \)  -o \( ! -e "${CACHED_DEV}" \) ]; then
+  echo "Preparing cached data."
+  python prepare_roberta_data.py --data_type textual --roberta_path ${DATA_DIR}/roberta.large --input_path ${DATA_DIR} --output_dir ${DATA_DIR}
+  fi
+fi
